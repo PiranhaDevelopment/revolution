cb126351899d34c41285f19888997097d137fb41
PiranhaDevelopment <78592864+PiranhaDevelopment@users.noreply.github.com>
2021-02-23 09:49:16 +0000

    classic mails

---
+++ mails/pt/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+$_LANGMAIL['Package in transit'] = '';
+$_LANGMAIL['Your order has been changed'] = '';
+$_LANGMAIL['Product out of stock'] = '';
+$_LANGMAIL['New order : #%d - %s'] = '';
+$_LANGMAIL['New return from order #%d - %s'] = '';
+$_LANGMAIL['Stock coverage'] = '';
+$_LANGMAIL['Product available'] = '';
+$_LANGMAIL['Message from %1$s %2$s'] = '';
+
+?>
